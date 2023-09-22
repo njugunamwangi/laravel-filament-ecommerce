b532eb671d2a776084e8ff6666f64da6982c73f7
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-23 01:50:45 +0300

    Spatie Permissions & Roles

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-## Laravel Mix
+## About Project
 
-Laravel + Mix
+An e-commerce site using Laravel 10, Filament V3 and Laravel Mix
+
+## Plugins
+
+- [Activity Log](https://github.com/pxlrbt/filament-activity-log)
+- [Media Library](https://filamentphp.com/plugins/filament-spatie-media-library)
+- [Spatie Roles Permissions](https://filamentphp.com/plugins/tharinda-rodrigo-spatie-roles-permissions)
+
+## Tech Stack
+
+- [Laravel](https://laravel.com)
+- [Filament V3](https://filamentphp.com/)
+- [TailwindCSS](https://tailwindcss.com/)
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -51,6 +52,9 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+            ->plugin(
+                FilamentSpatieRolesPermissionsPlugin::make()
+            )
             ->authMiddleware([
                 Authenticate::class,
             ]);
