5fd6899fef893ddbef178f7888135e857f8834ef
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-23 23:27:51 +0300

    Build

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -7,15 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Brand extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
     use SoftDeletes;
+    use HasSlug;
 
     public const CREATED_AT = null;
     public const UPDATED_AT = null;
 
-    protected $fillable = ['brand', 'description'];
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('brand')
+            ->saveSlugsTo('slug');
+    }
+
+    protected $fillable = ['brand', 'slug', 'description'];
 }
