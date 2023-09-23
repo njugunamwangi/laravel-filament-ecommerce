06eee1368b36d785bf5ac030ef299a63edb688b4
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-23 12:57:53 +0300

    Category model

---
+++ app/Models/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Category extends Model
+{
+    use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+    protected $fillable = ['category', 'slug', 'description', 'parent_id'];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('category')
+            ->saveSlugsTo('slug');
+    }
+
+    public function children() : HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent() :BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+}
---
+++ database/migrations/2023_09_23_092152_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category', 256);
+            $table->string('slug', 256);
+            $table->longText('description')->nullable();
+            $table->softDeletes();
+            $table->unsignedBigInteger('parent_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
