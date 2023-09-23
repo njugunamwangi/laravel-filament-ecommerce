1dd280071d849d5e946ff34db711d13dd053604a
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-23 13:29:44 +0300

    Tag model

---
+++ app/Models/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Tag extends Model
+{
+    use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+
+    protected $fillable = ['tag', 'slug', 'description'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('category')
+            ->saveSlugsTo('slug');
+    }
+}
