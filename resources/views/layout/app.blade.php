7aacb211378a010055a6a3dc5bdc3de34fc9fadb
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-24 20:32:36 +0300

    App Layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>E-Commerce</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+</head>
+    <body class="antialiased">
+        {{ $slot }}
+    </body>
+</html>
