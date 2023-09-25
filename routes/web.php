9b2de889f29f9c454a47e140a4f81c67f96c95bc
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-25 03:06:44 +0300

    Modify routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/{product:slug}', [ProductController::class, 'view'])->name('view');
