91b2a9e3efaa5c6ed24ead315e509acbc06e57c4
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-26 18:34:53 +0300

    User login & signup

--- frontend/src/views/auth/Login.jsx
+++ frontend/src/views/auth/Login.jsx
@@ -1,63 +1,50 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+import Header from "../components/Header.jsx";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import {useRef, useState} from "react";
+import axiosClient from "../../axios.js";
 
 export default function Login() {
-    const onSubmit = () => {
-        console.log("Signed up")
+    const { showToast } = useStateContext({})
+
+    const { setUser, setToken } = useStateContext()
+
+    const [ loading, setLoading ] = useState(false)
+
+    const navigate = useNavigate()
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const onSubmit = (ev) => {
+        ev.preventDefault()
+        setLoading(true)
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        axiosClient.post('/login', payload)
+            .then(({data}) => {
+                navigate('/')
+                setUser(data.user)
+                setToken(data.token)
+                setLoading(false)
+            })
+            .catch((error) => {
+                if (error.response.status === 422) {
+                    showToast(error.response.data.error, 'error')
+                    setLoading(false)
+                }
+            })
     }
 
     return (
         <>
-            <header className="mb-8 border-b">
-                <div className="mx-auto flex max-w-screen-2xl items-center justify-between px-4 md:px-8">
-                    <a href="/" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
-                        <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-                        </svg>
+            <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+                <Header />
 
-                        Flowrift
-                    </a>
-
-                    <nav className="hidden gap-12 lg:flex 2xl:ml-16">
-                        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Collections</a>
-                        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Sale</a>
-                        <a href="#" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">About</a>
-                    </nav>
-
-                    <div className="flex divide-x border-r sm:border-l">
-                        <a href="#" className="hidden h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:flex sm:h-20 sm:w-20 md:h-24 md:w-24">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                            </svg>
-
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Wishlist</span>
-                        </a>
-
-                        <a href="/log-in" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
-
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Account</span>
-                        </a>
-
-                        <a href="#" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                            </svg>
-
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
-                        </a>
-
-                        <button type="button" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24 lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-                            </svg>
-
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Menu</span>
-                        </button>
-                    </div>
-                </div>
-            </header>
+            </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -75,6 +62,7 @@ export default function Login() {
                                 type="email"
                                 autoComplete="email"
                                 required
+                                ref={emailRef}
                                 placeholder="joe@shields.com"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
@@ -86,6 +74,11 @@ export default function Login() {
                             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                 Password
                             </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    Forgot password?
+                                </a>
+                            </div>
                         </div>
                         <div className="mt-2">
                             <input
@@ -95,6 +88,7 @@ export default function Login() {
                                 autoComplete="current-password"
                                 required
                                 placeholder="********"
+                                ref={passwordRef}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -105,14 +99,14 @@ export default function Login() {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign In
+                            Sign in
                         </button>
                     </div>
                 </form>
 
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?{' '}
-                    <NavLink to="/sign-up" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                    <NavLink to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                         Sign Up
                     </NavLink>
                 </p>
