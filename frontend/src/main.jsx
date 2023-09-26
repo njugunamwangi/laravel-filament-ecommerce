8a3d181d88e9ff7d3256eebb2cf19ae51ac7277f
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-26 18:33:03 +0300

    Wrap context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./router.jsx";
+import {ContextProvider} from "./views/contexts/ContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <ContextProvider>
+          <RouterProvider router={router} />
+      </ContextProvider>
   </React.StrictMode>,
 )
