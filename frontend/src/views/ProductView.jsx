d06c69a7caaddf08b1760d2bbddd01bfb899db37
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-27 02:10:10 +0300

    Header buttons

--- frontend/src/views/ProductView.jsx
+++ frontend/src/views/ProductView.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import axiosClient from "../axios.js";
 import Header from "./components/Header.jsx";
 import Loading from "./core/Loading.jsx";
+import {useStateContext} from "./contexts/ContextProvider.jsx";
 
 export default function ProductView() {
     const { slug } = useParams()
@@ -10,6 +11,17 @@ export default function ProductView() {
     const [ product, setProduct ] = useState([])
 
     const [ loading, setLoading ] = useState(false)
+
+    const { user, setUser, token, setToken } = useStateContext()
+
+    if(token) {
+        useEffect(() => {
+            axiosClient.get('/me')
+                .then(({data}) => {
+                    setUser(data)
+                })
+        }, [])
+    }
 
     useEffect(() => {
         setLoading(true)
@@ -109,7 +121,7 @@ export default function ProductView() {
                                             <div className="flex flex-wrap gap-4">
                                                 {product.colors.map(color => (
                                                     <button key={color.id} type="button"
-                                                            className="flex h-8 w-12 items-center justify-center rounded-md border bg-white text-center text-sm font-semibold text-gray-800 transition duration-100 hover:bg-gray-100 active:bg-gray-200">{color.color}
+                                                            className="flex h-8 w-24 items-center justify-center rounded-md border bg-white text-center text-sm font-semibold text-gray-800 transition duration-100 hover:bg-gray-100 active:bg-gray-200">{color.color}
                                                     </button>
                                                 ))}
                                             </div>
@@ -156,14 +168,16 @@ export default function ProductView() {
                                            className="inline-block flex-1 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 sm:flex-none md:text-base">Add
                                             to cart</a>
 
-                                        <a href="#"
-                                           className="inline-block rounded-lg bg-gray-200 px-4 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                                                 viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
-                                            </svg>
-                                        </a>
+                                        {user && token && (
+                                            <a href="#"
+                                               className="inline-block rounded-lg bg-gray-200 px-4 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                                     viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                                                </svg>
+                                            </a>
+                                        )}
                                     </div>
                                 </div>
                             </div>
--- frontend/src/views/components/Header.jsx
+++ frontend/src/views/components/Header.jsx
@@ -1,16 +1,19 @@
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 import {useEffect} from "react";
 import axiosClient from "../../axios.js";
+import {Navigate} from "react-router-dom";
 
 export default function Header() {
     const { user, setUser, token, setToken } = useStateContext()
 
-    useEffect(() => {
-        axiosClient.get('/me')
-            .then(({ data }) => {
-                setUser(data)
-            })
-    }, [])
+    if(token) {
+        useEffect(() => {
+            axiosClient.get('/me')
+                .then(({data}) => {
+                    setUser(data)
+                })
+        }, [])
+    }
 
     return (
         <>
@@ -31,30 +34,50 @@ export default function Header() {
                     </nav>
 
                     <div className="flex divide-x border-r sm:border-l">
-                        <a href="#" className="hidden h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:flex sm:h-20 sm:w-20 md:h-24 md:w-24">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                            </svg>
+                        {user && token && (
+                            <>
+                                <a href="/wishlist" className="hidden h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:flex sm:h-20 sm:w-20 md:h-24 md:w-24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                    </svg>
 
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Wishlist</span>
-                        </a>
+                                    <span className="hidden text-xs font-semibold text-gray-500 sm:block">Wishlist</span>
+                                </a>
 
-                        <a href="/login" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
+                                <a href="/account" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    </svg>
 
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">
-                                Account
-                            </span>
-                        </a>
+                                    <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                                        Account
+                                    </span>
+                                </a>
+                            </>
+                        )}
+
+                        {!token && (
+                            <a href="/login" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                </svg>
+
+                                <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                                    Account
+                                </span>
+                            </a>
+                        )}
+
 
                         <a href="#" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                             </svg>
 
-                            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
+                            <div className="flex gap-2">
+                                <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
+                                <span className="hidden text-xs font-semibold text-gray-500 sm:block">0</span>
+                            </div>
                         </a>
 
                         <button type="button" className="flex h-12 w-12 flex-col items-center justify-center gap-1.5 transition duration-100 hover:bg-gray-100 active:bg-gray-200 sm:h-20 sm:w-20 md:h-24 md:w-24 lg:hidden">
