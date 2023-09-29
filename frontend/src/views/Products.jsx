35de7cde15cd39ec2915da0e24fe060091555188
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-29 15:42:56 +0300

    Pagination on products page

--- frontend/src/views/Products.jsx
+++ frontend/src/views/Products.jsx
@@ -3,19 +3,32 @@ import ProductCard from "./components/cards/ProductCard.jsx";
 import {useEffect, useState} from "react";
 import axiosClient from "../axios.js";
 import Loading from "./core/Loading.jsx";
+import Pagination from "./core/Pagination.jsx";
 
 export default function Products() {
     const [ products, setProducts ] = useState([])
 
     const [ loading, setLoading ] = useState(false)
 
-    useEffect(() => {
+    const [ meta, setMeta ] = useState({})
+
+    const onPageClick = (link) => {
+        getProducts(link.url)
+    }
+
+    const getProducts = (url) => {
+        url = url || "/products"
         setLoading(true)
-        axiosClient.get(`/products`)
+        axiosClient.get(url)
             .then(({ data }) => {
                 setProducts(data.data)
+                setMeta(data.meta)
                 setLoading(false)
             })
+    }
+
+    useEffect(() => {
+        getProducts()
     }, [])
     return (
         <>
@@ -30,11 +43,12 @@ export default function Products() {
                                 <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">All Products</h2>
                             </div>
 
-                            <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
+                            <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4 mb-4">
                                 {products.map(product => (
                                     <ProductCard product={product} key={product.id} />
                                 ))}
                             </div>
+                            <Pagination meta={meta} onPageClick={onPageClick} />
                         </div>
                     </div>
                 </>
